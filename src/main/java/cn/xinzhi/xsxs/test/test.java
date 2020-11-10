50c20ccf05cd888e3eed456b8589cb399a0779be
kzx616098 <56617865+kzx616098@users.noreply.github.com>
2020-11-10 10:08:11 +0800

    Update test.java

--- src/main/java/cn/xinzhi/xsxs/test/test.java
+++ src/main/java/cn/xinzhi/xsxs/test/test.java
@@ -12,17 +12,17 @@ import com.aliyuncs.profile.DefaultProfile;
 class SendSms {
     public static void main(String[] args) {
         RandomUser ru = new RandomUser();
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4Fkcsm7BQij9BZGy7yeu", "g0HajyMArMqFCl2gUychv01o6waOdY");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
         IAcsClient client = new DefaultAcsClient(profile);
         CommonRequest request = new CommonRequest();
         request.setMethod(MethodType.POST);
-        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setDomain("");
         request.setVersion("2017-05-25");
         request.setAction("SendSms");
         request.putQueryParameter("RegionId", "cn-hangzhou");
         request.putQueryParameter("PhoneNumbers", "");
         request.putQueryParameter("SignName", "心诉星说");
-        request.putQueryParameter("TemplateCode", "SMS_181861153");
+        request.putQueryParameter("TemplateCode", "");
         request.putQueryParameter("TemplateParam", "{\"code\":" + ru.randomId() + "}");
         try {
             CommonResponse response = client.getCommonResponse(request);
